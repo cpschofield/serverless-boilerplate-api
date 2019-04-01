ddfdd1839ba61662763046180132d34eee8181fa
Chris <chris@simble.io>
2019-04-02 08:36:01 +1100

    added in middleware handler to user controller temporarily

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,10 +1,10 @@
 import { connectToDB } from '../service';
-// import User from '../model';
+import { User } from '../model';
 
 /**
  * Functions
  */
-module.exports.getUsers = async () => {
+const getUsers = async () => {
   try {
     await connectToDB();
     const users = await User.find({});
@@ -21,3 +21,25 @@ module.exports.getUsers = async () => {
     };
   }
 };
+
+const someMW = async (event, ctx) => {
+  // do something
+  console.log('==> MW did something');
+  // returns mutated event and contexts
+  return Promise.resolve({ event, ctx });
+};
+
+export const middlewareRunnerX = async (handler, middleware = []) => async (event, ctx) => {
+  console.log('run middleware');
+  const results = awaitmiddleware.reduce(async (promiseChain, currentTask) => {
+    const chainResults = await promiseChain;
+    const currentResult = await currentTask(event, ctx);
+    console.log('==> current task resolved');
+    return [...chainResults, currentResult];
+  }, Promise.resolve([]));
+
+  console.log(results);
+  return handler();
+};
+
+module.exports.getUsers = middlewareRunnerX(getUsers, [someMW, someMW]);
