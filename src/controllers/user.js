53d5b65422515bd4370c2c895c0f4da3b5e3cc87
Mitchell Rowling <mitchell.rowling@oohmedia.com.au>
2019-04-02 22:27:09 +1100

    Rework so that it runs handler

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,42 +4,41 @@ import { User } from '../model';
 /**
  * Functions
  */
-const getUsers = async () => {
+export const getUsers = async () => {
   try {
     await connectToDB();
     const users = await User.find({});
     return {
       statusCode: 200,
-      body: JSON.stringify(users),
+      body: JSON.stringify(users)
     };
   } catch (e) {
     console.log(e);
     return {
       statusCode: e.statusCode || 500,
       headers: { 'Content-Type': 'text/plain' },
-      body: JSON.stringify({ message: e.message }),
+      body: JSON.stringify({ message: e.message })
     };
   }
 };
 
-const someMW = async (event, ctx) => {
+const someMW = (name) => async (event, ctx) => {
   // do something
-  console.log('==> MW did something');
+  console.log(`==> MW ${name} did something`);
   // returns mutated event and contexts
   return Promise.resolve({ event, ctx });
 };
 
-export const middlewareRunnerX = async (handler, middleware = []) => async (event, ctx) => {
+const composeMiddleware = (handler, middleware = []) => async (event, ctx) => {
   console.log('run middleware');
-  const results = awaitmiddleware.reduce(async (promiseChain, currentTask) => {
+  const results = await middleware.reduce(async (promiseChain, currentTask, index) => {
     const chainResults = await promiseChain;
     const currentResult = await currentTask(event, ctx);
-    console.log('==> current task resolved');
-    return [...chainResults, currentResult];
+    console.log(`==> current task ${index} resolved`);
+    return [ ...chainResults, currentResult ];
   }, Promise.resolve([]));
-
-  console.log(results);
+  console.log('END MIDDLEWARE');
   return handler();
 };
 
-module.exports.getUsers = middlewareRunnerX(getUsers, [someMW, someMW]);
+module.exports.getUsers = async (event, ctx) => composeMiddleware(getUsers, [ someMW(0), someMW(1) ])(event, ctx);
